987b69cdbd77cee85cac39a1534f09bdeb42fbbf
mailtomainak <mailtomainak@gmail.com>
2018-11-21 21:56:48 +0530

    Move sensitive data to env.

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
     env: process.env.NODE_ENV || 'development',
     allowedCORSUrl:['http://localhost','http://lvh.me'],
     db: {
-		uri: process.env.MONGODB_URI || 'mongodb://dev-db-user:7ad80c07-34b0-46f4-abb2-71e16cb6fe0a@ds149437.mlab.com:49437/datastore-mongo-dev',
+		uri: process.env.MONGODB_URI,
 	},
 }
 
